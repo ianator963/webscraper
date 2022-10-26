391f3f22f69dbd289c0980892d9d64f769c8e874
ianator963 <ianator963@gmail.com>
2022-10-26 12:34:16 -0400

    finished the scraper, prints all python related jobs and the application links, inserted and reformated comments

--- scraper.py
+++ scraper.py
@@ -17,18 +17,41 @@ results = soup.find(id="ResultsContainer")
 # print(results.prettify())
 #commented out code above takes the raw input and makes it slightly more legible but not good enough: continue
 
-# job_elements = results.find_all("div", class_="card-content")
-# for job_element in job_elements:
-#     title_element = job_element.find("h2", class_="title")
-#     company_element = job_element.find("h3", class_="company")
-#     location_element = job_element.find("p", class_="location")
-#     print(title_element.text.strip())
-#     print(company_element.text.strip())
-#     print(location_element.text.strip())
-#     ##3 lines above can be written without ".text" and ".strip"
-#     ##".text" removes all the html tags when print and ".strip" removes all the unnecessary whitespace
-#     print()
+'''
+job_elements = results.find_all("div", class_="card-content")
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    ##3 lines above can be written without ".text" and ".strip"
+    ##".text" removes all the html tags when print and ".strip" removes all the unnecessary whitespace
+    print()
+'''
 
 
-python_jobs = results.find_all("h2", string="Python")
-print(python_jobs)
+python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+#defines the parent for the python jobs so that you can see the actual text instead of just the header
+
+job_elements = results.find_all("div", class_="card-content")
+for job_element in python_job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    #runs the same thing as above, but for the python job elements.
+    '''links = job_element.find_all("a")
+	for link in links:
+		link_url = link["href"]
+		print(f"Apply here: {link_url}\n")'''
+	#prints the links in the child fields from the python job elements in the <h2> tags
+    link_url = job_element.find_all("a")[1]["href"]
+    print(f"Apply here: {link_url}\n")
+    #changes the above code to print just the application link and not the information link
